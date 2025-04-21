60ca7fa9ac5b3a9f875e4ee13c6d7eabe8e2f227
dungpb <badung.phan@vietnamnet.vn>
2025-04-21 10:01:24 +0700

    update config scheme

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import type { API } from 'homebridge';
 
-import { HomebridgeMyHomePlatform } from './platform.js';
+import { HomebridgeTasmotaHeaterCooler } from './platform.js';
 import { PLATFORM_NAME } from './settings.js';
 
 /**
  * This method registers the platform with Homebridge
  */
 export default (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, HomebridgeMyHomePlatform);
+  api.registerPlatform(PLATFORM_NAME, HomebridgeTasmotaHeaterCooler);
 };
--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeDungpb';
+export const PLATFORM_NAME = 'HomebridgeTasmotaHeaterCooler';
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-dungpb';
+export const PLUGIN_NAME = 'homebridge-tasmota-heater-cooler';
